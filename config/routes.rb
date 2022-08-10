58cca6b15b55bf20171c7ce674545e52555090b7
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-10 14:24:02 -0400

    restaurant controller routes, actions, and views done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # A visitor can see the list of all restaurants.
+  get 'restaurants', to: 'restaurants#index'
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # A visitor can add a new review to a restaurant
+  # get 'restaurants/:id/reviews/new'
+  # post 'restaurants/:id/reviews'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_10_155442) do
-  create_table "ratings", force: :cascade do |t|
-    t.text "content"
-    t.integer "restaurant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["restaurant_id"], name: "index_ratings_on_restaurant_id"
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -37,6 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_155442) do
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
-  add_foreign_key "ratings", "restaurants"
   add_foreign_key "reviews", "restaurants"
 end
